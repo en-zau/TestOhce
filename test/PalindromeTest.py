42f66414894145875b363e210fe4102cdf2deb36
en-zau <enzo.gouerec@epsi.fr>
2023-01-31 15:27:20 +0100

    rework test for salutation with ohcebuilder

--- test/PalindromeTest.py
+++ test/PalindromeTest.py
@@ -1,25 +1,30 @@
 import unittest
-from src.Ohce import Ohce
+from parameterized import parameterized
+
 
 class PalindromeTest(unittest.TestCase):
     def test_renvoi_miroir(self):
-        chaine = "oula"
+        chaine = "toto"
 
-
-        ohce = Ohce()
+        ohce = OhceBuilder.default()
         chaine_renvoye = ohce.revert(chaine)
 
         self.assertIn(chaine[::-1], chaine_renvoye)
 
-    def test_palindrome(self):
-        chaine = "radar"
-        ohce = Ohce()
-        chaine_renvoye = ohce.revert(chaine)
-        self.assertIn(chaine_renvoye, chaine)
+    @parameterized.expand([
+        [LangueAnglaise(), "Well done"],
+        [LangueFr(), "Bien dit"],
+    ])
+    def test_palindrome(self, languechoisie, bien_dit):
+        palindrome = "radar"
 
-        resultat_apres_palindrome = ohce.palindrome(chaine)
-        self.assertIn(chaine + "Bien dit", resultat_apres_palindrome)
+        ohce = OhceBuilder().langue(languechoisie).build()
+
+        resultat = ohce.palindrome(palindrome)
+        self.assertIn(palindrome, resultat)
+
+        resultat_apres_palindrome = resultat[len(palindrome):len(resultat)]
+        self.assertIn(palindrome + bien_dit, resultat_apres_palindrome)
 
 
 if __name__ == '__main__':
-    unittest.main()
